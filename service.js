95ceb43a02c954df93d6a9cfaf873d896b8a7c9a
Shadawn <esteraza@yandex.ru>
2021-09-20 12:10:55 +0300

    fixed some service stuff

--- service.js
+++ service.js
@@ -3,8 +3,7 @@ const Service = require('node-windows').Service;
 const svc = new Service({
   name: 'A1 - Github Releaser',
   description: 'Automatic release builder for Github',
-  // eslint-disable-next-line no-undef
-  script: __dirname + '\\index.js',
+  script: __dirname + '\\src\\index.js',
   nodeOptions: [
     '--harmony',
     '--max_old_space_size=4096',
@@ -14,9 +13,19 @@ const svc = new Service({
 });
 
 svc.on('install', () => {
+  console.log('Service installed');
   svc.start();
+});
+svc.on('start', () => {
   console.log('Service started');
 });
+svc.on('uninstall', () => {
+  console.log('Service uninstalled');
+});
 
-svc.install();
-console.log('Service installed');
+if (process.argv.length === 3 && process.argv[2] === 'uninstall') {
+  svc.uninstall();
+} else {
+  svc.install();
+}
+
